6161f2e47c1a57e36f826bbfd12fc1db295689fd
VINUTHASHREE N N <54951101+vinutha06@users.noreply.github.com>
2021-09-18 20:50:00 +0530

    day2 assignment submitted

---
+++ day2.java
@@ -0,0 +1,9 @@
+public class AddNumbers{
+  public static void main(String[] args) {
+  System.out.println("Addition of two numbers!");
+  int a = Integer.parseInt(args[0]);
+  int b = Integer.parseInt(args[1]);
+  int sum = a + b;
+  System.out.println("Sum: " + sum);
+  }
+}
